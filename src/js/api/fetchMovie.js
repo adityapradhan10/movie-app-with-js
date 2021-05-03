c3db1efbe789693b796d60a7a10f8ae59b5a49e4
Aditya Pradhan <adipradhan2@gmail.com>
2021-05-03 17:33:48 +0530

    Added single movie feature

--- src/js/api/fetchMovie.js
+++ src/js/api/fetchMovie.js
@@ -1,1 +1,24 @@
-// http://www.omdbapi.com/?i=tt2591814&apikey=acc4801f -> Detail
+const axios = require("axios").default;
+
+export const fetchMovie = async (imdbID) => {
+  const URL = `http://www.omdbapi.com`;
+  let mMovie = "";
+
+  console.log("hsabd");
+  try {
+    mMovie = await axios({
+      method: "get",
+      url: URL,
+      params: {
+        i: imdbID,
+        apikey: process.env.API_KEY,
+      },
+    });
+
+    mMovie = await mMovie.data;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return mMovie;
+};
--- src/js/single.js
+++ src/js/single.js
@@ -1,1 +1,29 @@
 import "../scss/style.scss";
+import { fetchMovie } from "./api/fetchMovie";
+
+const singleMovie = document.querySelector("#single-movie");
+const imdbLink = document.querySelector("#imdb-link");
+imdbLink.href = `https://www.imdb.com/title/${imdbID}/`;
+
+const mMovie = fetchMovie(imdbID);
+mMovie
+  .then((moive) => {
+    let posterUrl = moive.Poster;
+    let indexOfRes = posterUrl.lastIndexOf("3");
+    posterUrl =
+      posterUrl.substring(0, indexOfRes) +
+      "12" +
+      posterUrl.substring(indexOfRes + 1);
+
+    singleMovie.querySelector("#poster").src = posterUrl;
+    singleMovie.querySelector("#name").textContent = moive.Title;
+    singleMovie.querySelector("#year").textContent = moive.Year;
+    singleMovie.querySelector("#plot").textContent = moive.Plot;
+    singleMovie.querySelector("#director").textContent = moive.Director;
+    singleMovie.querySelector("#writer").textContent = moive.Writer;
+    singleMovie.querySelector("#type").textContent = moive.Type;
+    singleMovie.querySelector("#duration").textContent = moive.Runtime;
+    singleMovie.querySelector("#genre").textContent = moive.Genre;
+    singleMovie.querySelector("#actors").textContent = moive.Actors;
+  })
+  .catch((err) => console.log(err));
